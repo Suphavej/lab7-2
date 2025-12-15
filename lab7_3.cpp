0497fb4a8ed7590e57b78a49b9b2ebfbea7a5235
Suphavej <fatkimmmm@gmail.com>
2025-12-15 16:50:56 +0700

    sed laeww

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    if (x == 'A') {
+        return 'Z';
+    }
+    else if (x >= 'B' && x <= 'Z') {
+        return x - 1;
+    }
+    else {
+        return '0';
+    }
 }
 
 int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+    // Test Case
+    cout << before('A') << endl;
+    cout << before('B') << endl;
+    cout << before('P') << endl;
+    cout << before('T') << endl;
+    cout << before('Z') << endl;
+    cout << before('a') << endl;
+    cout << before('0') << endl;
+    cout << before('c') << endl;
+
+    return 0;
 }
